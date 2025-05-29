1ba305d750247f3be16ab24591f75427fdabd111
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-05-30 02:07:20 +0530

    allowed cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ const app = express();
 app.use(clerkMiddleware());
 // Middleware to parse the JSON data in the request body
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({ origin: "https://laundryexpress25.netlify.app" }));
 
 connectDB();
 
